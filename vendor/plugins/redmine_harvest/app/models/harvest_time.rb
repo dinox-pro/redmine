9f2e28e50e03061905be9ac79e4ade586d73a43c
Jim Mulholland <jim@jim-mb-pro-2.local>
2009-11-13 01:27:38 -0600

    Working on importing data

--- vendor/plugins/redmine_harvest/app/models/harvest_time.rb
+++ vendor/plugins/redmine_harvest/app/models/harvest_time.rb
@@ -12,4 +12,24 @@ class HarvestTime < ActiveRecord::Base
     custom_value = user.custom_values.detect {|v| v.custom_field_id == Setting.plugin_redmine_harvest['harvest_user_id'].to_i}
     harvest_user_id = custom_value.value.to_i if custom_value
   end
+  
+  def self.import_time(project)
+    harvest_project_id = self.project_id(project)
+    # From date of last job for project;  Default to 1 year ago.
+    from_date = HarvestTime.minimum(:created_at, :conditions=>{:project_id => project.id}) || 1.year.ago
+    to_date = Time.now
+    
+    harvest_user_custom_field_id = Setting.plugin_redmine_harvest['harvest_user_id']
+    
+    Harvest.report.project_entries(harvest_project_id, from_date, to_date).each do |entry|
+      entry.project_id = project.id
+      
+      # Find the Redmine user id through the CustomValue data
+      user_custom_value = CustomValue.find_by_value_and_custom_field_id(entry.user_id, harvest_user_custom_field_id)
+      entry.user_id = user_custom_value ? user_custom_value.customized.id : nil
+
+    end
+    
+    
+  end
 end
